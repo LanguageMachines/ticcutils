e7ee9e5dc5be3b8c694c41c1a67d45814045fb21
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-10-10 11:58:45 +0000

    adapted copyright headers
    git-svn-id: https://ilk.uvt.nl/svn/sources/libticcutils/trunk@15267 12f355fe-0486-481a-ad91-c297ab22b4e3

--- src/LogStream.cxx
+++ src/LogStream.cxx
@@ -6,14 +6,14 @@
   ILK   - Tilburg University
   CLiPS - University of Antwerp
  
-  This file is part of timbl
+  This file is part of ticcutils
 
-  timbl is free software; you can redistribute it and/or modify
+  ticcutils is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 3 of the License, or
   (at your option) any later version.
 
-  timbl is distributed in the hope that it will be useful,
+  ticcutils is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
