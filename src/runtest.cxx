9114c077d2cc94f7fcbce3c964ed85ed7659aa62
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2017-05-15 11:11:51 +0200

    changed the new test

--- src/runtest.cxx
+++ src/runtest.cxx
@@ -211,11 +211,11 @@ void test_opts_basic(){
   assertEqual( v.size(), 2 );
   CL_Options opts15;
   opts15.set_debug(true);
-  opts15.parse_args( "--fout=goed=mis --jan=gek" );
-  assertEqual( opts15.toString(), "--fout=goed=mis --jan=gek" );
+  opts15.parse_args( "--fout=goed\\mis --jan=gek" );
+  assertEqual( opts15.toString(), "--fout=goed\\mis --jan=gek" );
   string res;
   opts15.extract("fout", res );
-  assertEqual( res, "goed=mis" );
+  assertEqual( res, "goed\\mis" );
   opts15.extract("jan", res );
   assertEqual( res, "gek" );
 }
