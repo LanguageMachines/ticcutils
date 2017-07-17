5e4ed53a727982fae3fe229cd53c04a4ce6ce1a7
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2017-07-17 15:08:52 +0200

    improved configuration: Don't built without Regex support. Preferably native; or else Boost

--- src/FileUtils.cxx
+++ src/FileUtils.cxx
@@ -37,8 +37,12 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include "config.h"
-#ifdef HAVE_BOOST_REGEX
-#include <boost/regex.hpp>
+#if HAVE_WORKING_REGEX == 2
+#  include <boost/regex.hpp>
+#  define regex boost::regex
+#  define regex_error boost::regex_error
+#else
+#  include <regex>
 #endif
 #include "ticcutils/StringOps.h"
 #include "ticcutils/FileUtils.h"
@@ -129,8 +133,7 @@ namespace TiCC {
     return result;
   }
 
-#ifdef HAVE_BOOST_REGEX
-  void gatherFilesMatch( const string& dirName, const boost::regex& match,
+  void gatherFilesMatch( const string& dirName, const regex& match,
 			 vector<string>& result, bool recurse ){
     DIR *dir = opendir( dirName.c_str() );
     if ( !dir ){
@@ -146,7 +149,7 @@ namespace TiCC {
 	  gatherFilesMatch( fullName, match, result, recurse );
 	}
       }
-      else if ( boost::regex_search( name, match ) ){
+      else if ( regex_search( name, match ) ){
 	result.push_back( fullName );
       }
       entry = readdir( dir );
@@ -182,7 +185,7 @@ namespace TiCC {
     vector<string> result;
     string reg = wildToRegExp( wild );
     try {
-      boost::regex rx( reg );
+      regex rx( reg );
       if ( isFile( name ) ){
 	// it is just 1 file
 	string::size_type pos = name.rfind( "/" );
@@ -193,7 +196,7 @@ namespace TiCC {
 	else {
 	  fname = name;
 	}
-	if ( boost::regex_search( fname, rx ) )
+	if ( regex_search( fname, rx ) )
 	  result.push_back( name );
 	return result;
       }
@@ -204,7 +207,7 @@ namespace TiCC {
       }
       gatherFilesMatch( name, rx, result, recurse );
     }
-    catch( boost::regex_error& e ){
+    catch( regex_error& e ){
       string mess = "TiCC::searchFilesMatch: invalid regexp: ";
       mess += e.what();
       throw runtime_error( mess );
@@ -214,15 +217,6 @@ namespace TiCC {
     }
     return result;
   }
-#else
-  vector<string> searchFilesMatch( const string& name,
-				   const string& match,
-				   bool recurse ){
-    cerr << "REGEXP support not available" << endl;
-    cerr << "  attempting lame extension matching instead" << endl;
-    return searchFilesExt( name, match, recurse );
-  }
-#endif
 
   bool createTruePath( const string& path ){
     // attempt to open a path /a/b/c/ from an expression like:
