aa97f29be5f3a165566795edec6bb5c93438ab88
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-11-20 16:43:24 +0100

    oops. removed use of valgrind from test

--- src/tst.sh
+++ src/tst.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-valgrind ./.libs/runtest -ffalse +t true --test=test -d $topsrcdir/tests/  -q --raar blaat arg1 arg2
+./.libs/runtest -ffalse +t true --test=test -d $topsrcdir/tests/  -q --raar blaat arg1 arg2
 
 #./testlogstream
