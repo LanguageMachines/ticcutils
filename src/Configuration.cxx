d1d9e786c9ee3d78a6dcd2eba80298358b5bcb9a
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2021-03-01 12:54:50 +0100

    small improvement

--- src/Configuration.cxx
+++ src/Configuration.cxx
@@ -438,7 +438,7 @@ namespace TiCC {
     for ( const auto& s : sections ){
       // for every section, get all at-val pairs
       auto avs = in.lookUpAll( s );
-      for ( const auto av : avs ){
+      for ( const auto& av : avs ){
 	// merge every at-val in the wanted section
 	if ( !override
 	     &&  myMap[s].find( av.first ) != myMap[s].end() ){
