537b8b7c7fb02755995124d1185e51dfbb98a421
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2023-02-16 17:54:24 +0100

    improved template experiment

--- src/runtest.cxx
+++ src/runtest.cxx
@@ -1154,45 +1154,29 @@ struct has_##NAME {			\
 MAKE_FUN_CHECK( string_fun )
 MAKE_FUN_CHECK( unistring_fun )
 
-template<typename C, typename Ret, typename... Args>
-struct has_string_fun<C, Ret(Args...)> {
-private:
-  template<typename T>
-  static constexpr auto check(T*)
-    -> typename
-    std::is_same<
-  decltype( std::declval<T>().string_fun( std::declval<Args>()... ) ),
-  Ret    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  >::type;  // attempt to call it and see if the return type is correct
-
-  template<typename>
-  static constexpr std::false_type check(...);
-
-  typedef decltype(check<C>(0)) type;
-
-public:
-  static constexpr bool value = type::value;
+#define MAKE_AGAIN( FUN ) \
+template<typename C, typename Ret, typename... Args> \
+struct has_##FUN<C, Ret(Args...)> { \
+private: \
+  template<typename T> \
+  static constexpr auto check(T*) \
+    -> typename			  \
+    std::is_same<							\
+    decltype( std::declval<T>().FUN( std::declval<Args>()... ) ), \
+    Ret \
+  >::type; \
+						\
+  template<typename>				\
+  static constexpr std::false_type check(...);	\
+						\
+  typedef decltype(check<C>(0)) type;		\
+						\
+public:						\
+ static constexpr bool value = type::value;	\
 };
 
-template<typename C, typename Ret, typename... Args>
-struct has_unistring_fun<C, Ret(Args...)> {
-private:
-  template<typename T>
-  static constexpr auto check(T*)
-    -> typename
-    std::is_same<
-  decltype( std::declval<T>().unistring_fun( std::declval<Args>()... ) ),
-  Ret    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  >::type;  // attempt to call it and see if the return type is correct
-
-  template<typename>
-  static constexpr std::false_type check(...);
-
-  typedef decltype(check<C>(0)) type;
-
-public:
-  static constexpr bool value = type::value;
-};
+MAKE_AGAIN( string_fun )
+MAKE_AGAIN( unistring_fun )
 
 void test_templates(){
 
